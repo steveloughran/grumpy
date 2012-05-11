554412dd6b57253d36cded709a08b1edc3b562cd
Steve Loughran <stevel@hortonworks.com>
2012-05-10 23:05:10 -0700

    add more

---
+++ src/java/org/apache/hadoop/grumpy/GrumpyInit.groovy
@@ -0,0 +1,42 @@
+package java.org.apache.hadoop.grumpy
+
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.mapreduce.Job
+import groovy.util.logging.Commons
+
+/**
+ * This contains the init logic that sets up various Hadoop classes to be
+ * a bit more groovy
+ */
+@Commons
+class GrumpyInit {
+
+    static {
+        /** array assignment for job confs */
+        Configuration.metaClass.setAt = { k, v ->
+            set(k.toString(), v.toString())
+        }
+
+        Configuration.metaClass.getAt = { k ->
+            get(k)
+        }
+
+        /**
+         * Job array assignments propagate to the inner config
+         */
+        Job.metaClass.setAt = { k, v ->
+            configuration[k] = v
+        }
+
+        Job.metaClass.getAt = { k ->
+            configuration(k)
+        }
+
+        log.debug("Grumpy is now initialized")
+
+    }
+
+}
+
+
---
+++ src/java/org/apache/hadoop/grumpy/GrumpyJob.groovy
@@ -0,0 +1,8 @@
+
+
+package java.org.apache.hadoop.grumpy
+
+import org.apache.hadoop.mapreduce.Job
+
+class GrumpyJob extends Job {
+}
